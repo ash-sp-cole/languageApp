4f15a27124c641e51c61f4bd8dc4b46c2600e6c8
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-02-13 00:02:41 +0100

    formatiing

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -4,16 +4,19 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 const Body = (props) => {
 return(
 
-<div className="Display">
+<div >
 <Jumbotron>
 
-<h3>  Here is the weather for your city :</h3>
-<h2>{props.display}</h2>
-<h3>Current weather description is : {props.description}</h3>
-<h3> Current temperture (in celcius) is : {props.temp}</h3>
+<h1>  Here is the weather for your city :</h1>
+<h2 style={{textAlign:'center'}}>{props.display}</h2>
+<h2 style={{textAlign:'center'}}>{props.description}</h2>
+  <h2 style={{textAlign:'center'}}>{props.temp}*C</h2>
 <h4> Sunrise (GMT) : {props.sunrise}</h4>
 <h4>SunSet (GMT) : {props.sunset}</h4>
-<h2>Cloud cover right now is : {props.cloudCover}% </h2>
+<h2 style={{textAlign:'center'}}>Cloud cover right now is : {props.cloudCover}% </h2>
+
+
+
 </Jumbotron>
  
 </div>
